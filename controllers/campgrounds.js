e92001e30357c12de1a34e7682e6d4c341d1cac1
Yuvraj Singh <yuvrajsingh1619@gmail.com>
2024-04-04 15:09:47 +0530

    n

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,7 +1,7 @@
 const Campground = require('../models/campground');
-const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
-const mapBoxToken = process.env.MAPBOX_TOKEN;
-const geocoder = mbxGeocoding({ accessToken:mapBoxToken });
+// const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
+// const mapBoxToken = process.env.MAPBOX_TOKEN;
+// const geocoder = mbxGeocoding({ accessToken:mapBoxToken });
 const { cloudinary }= require('../cloudinary')
 
 
